4a2f2547bf1466275aa72a7b7554739873d3e16e
wignn <tigfiragnafatur1933@gmail.com>
2025-09-25 20:20:42 +0700

    Fix

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('tugas1')->group(function(){
+    Route::get('/profile/{name?}', function($name){
+        return view('tugas-1.user', ['name' => $name]);
+    })->name('profile');
+
+    Route::get('/menu', function(){
+        return view('tugas-1.menu');
+    })->name('menu');
+
+    Route::get('/{name?}', function($name = 'Guest') {
+        return 'ini adalah ' . $name;
+    });
+});
+
+
+Route::get('/health', function(){
+    return "OK";
+});
